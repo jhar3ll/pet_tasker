c4e54b4013354698885c1ade991a419112dbe8ab
jhar3ll <jharell.chambers@selu.edu>
2020-11-14 14:05:52 -0600

    time and date

--- db/migrate/20201110214743_create_tasks.rb
+++ db/migrate/20201110214743_create_tasks.rb
@@ -3,7 +3,6 @@ class CreateTasks < ActiveRecord::Migration[6.0]
     create_table :tasks do |t|
       t.string :description
       t.date :task_date
-      t.time :task_time
     
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -26,7 +26,6 @@ ActiveRecord::Schema.define(version: 2020_11_10_214743) do
   create_table "tasks", force: :cascade do |t|
     t.string "description"
     t.date "task_date"
-    t.time "task_time"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
