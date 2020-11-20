02bb007f6d7ae528513fcfb23382ca5eaabece2a
jhar3ll <jharell.chambers@selu.edu>
2020-11-19 20:15:40 -0600

    clean up

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,50 +1,30 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :update, :destroy]
 
-  # GET /tasks
   def index
-    @tasks = Task.all
-    render json: @tasks, except: [:created_at, :updated_at]
+    tasks = Task.all
+    render json: tasks, except: [:created_at, :updated_at]
   end
 
-  # GET /tasks/1
   def show
-    @task = Task.find_by(id: params[:id])
-    render json: @task, except: [:created_at, :updated_at]
+    task = Task.find_by(id: params[:id])
+    render json: task, except: [:created_at, :updated_at]
   end
 
-  # POST /tasks
   def create
-    @task = Task.new(task_params)
+    task = Task.new(task_params)
 
-    if @task.save
-      render json: @task, status: :created, location: @task
+    if task.save
+      render json: task, status: :created, location: task
     else
-      render json: @task.errors, status: :unprocessable_entity
+      render json: task.errors, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /tasks/1
-  def update
-    if @task.update(task_params)
-      render json: @task
-    else
-      render json: @task.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /tasks/1
   def destroy
-    @task.destroy
+    task.destroy
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_task
-      @task = Task.find(params[:id])
-    end
-
-    # Only allow a trusted parameter "white list" through.
     def task_params
       params.require(:task).permit(:description, :task_date, :task_time, :pet_name, :user_id)
     end
